b3dbceffdb17173beca6869463d7bf403f75e2f8
leahbateman@gmail.com Bateman <leahbateman@gmail.com>
2018-01-13 23:27:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+  var sum = a + b 
+  return sum
+}
+
+function subtract(a, b) {
+  var difference = a - b
+  return difference
+}
+
+function multiply(a, b) {
+  var product = a * b 
+  return product
+}
+
+function divide(a, b) {
+  var quotient = a / b 
+  return quotient
+}
+
+function inc(n) {
+  var incremented = n + 1
+  return incremented
+}
+
+function dec(n) {
+  var decremented = n - 1 
+  return decremented
+}
+
+function makeInt(n) {
+  var integer = n
+  return parseInt(integer,10)
+}
